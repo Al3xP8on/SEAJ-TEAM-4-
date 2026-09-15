452431932a0fcc4a0f7020292fc2b385a07f3ec5
Alex <Alexp8on@yahoo.com>
2026-09-15 15:00:09 +0000

    Added file back

--- starter/src/main/java/com/neueda/leap/Account.java
+++ starter/src/main/java/com/neueda/leap/Account.java
@@ -124,4 +124,4 @@ public class Account {
     private void setVersion(int version) { this.version = version; }
     private void setLastUpdated(LocalDateTime lastUpdated) { this.lastUpdated = lastUpdated; }
     private void setCashBalance(float cashBalance) { this.cashBalance = cashBalance; }
-}
+}S
