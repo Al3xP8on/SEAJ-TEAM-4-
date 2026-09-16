bf542639a5a58ee5ef541fb80151530a2fcda599
Samson Oloruntola <samson-oloruntola4@outlook.com>
2026-09-16 16:10:11 +0000

    feat: Add classes for current_prices table alongside exceptions.

---
+++ starter/src/main/java/com/neueda/leap/SymbolPriceValidator.java
@@ -0,0 +1,20 @@
+package com.neueda.leap;
+
+import java.math.BigDecimal;
+
+import com.neueda.leap.exceptions.InvalidPriceException;
+import com.neueda.leap.exceptions.InvalidSymbolPriceException;
+
+public class SymbolPriceValidator {
+    public void validateNewPrice(BigDecimal newPrice) throws InvalidPriceException {
+        if(newPrice == null || newPrice.compareTo(BigDecimal.ZERO) <= 0){
+            throw new InvalidPriceException("Invalid price: " + newPrice);
+        } 
+    }
+
+    public void validateSymbolPrice(SymbolPrice symbolPrice) throws InvalidSymbolPriceException {
+        if(symbolPrice == null){
+            throw new InvalidSymbolPriceException("SymbolPrice cannot be null.");
+        }
+    }
+}
